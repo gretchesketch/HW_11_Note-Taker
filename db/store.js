e02dc879f1bbfc050c0ff66a498c915ea867af80
gretchesketch <gretchenjensen92@gmail.com>
2022-05-16 09:46:26 -0400

    added a new js file to the db folder to handle the utility of the db

---
+++ db/store.js
@@ -0,0 +1,13 @@
+// utilities to handle the database
+
+const fs = require("fs");
+
+// this package will generate our unique ids
+const uuidv1 = require("uuid/v1");
+
+// ysing class to create an add note method
+class Sore {
+    addNote(note) {
+        const { title, text } = note;
+    }
+}
