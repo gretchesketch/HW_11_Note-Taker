a2569925287ea6b4e96f760a11ebdd3db4b71777
gretchesketch <gretchenjensen92@gmail.com>
2022-05-16 09:57:29 -0400

    added more to store js file

--- db/store.js
+++ db/store.js
@@ -2,12 +2,27 @@
 
 const fs = require("fs");
 
-// this package will generate our unique ids
+// this package will generate unique ids
 const uuidv1 = require("uuid/v1");
+const util = require("util");
 
-// ysing class to create an add note method
-class Sore {
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// using class to create an add note method
+class Store {
+    getNotes() {
+    return readFileAsync("db/db.json", "utf8")
+}
+
     addNote(note) {
         const { title, text } = note;
+
+        // if statment to require user input for both title and text box otherwise throw and error
+        if (!title || !text) {
+            throw new Error("tit;e and text are required.");
+        }
+
+        const newNote = {title, text, id: uuidv1() };
     }
 }
