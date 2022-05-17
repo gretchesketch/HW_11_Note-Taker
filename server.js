c73db23c9ce379acf12d77430a20a0714f00704b
gretchesketch <gretchenjensen92@gmail.com>
2022-05-17 14:55:13 -0400

    commented out something everythign still working but it wont deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // required packages
 const express = require("express");
-const router = require("./routes/htmlRoutes");
+//const router = require("./routes/htmlRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
 const apiRoutes = require("./routes/apiRoutes");
 
